f96ee47e9f844a0020f45b50c9e3283580eb5197
Makan <m.macalou522@gmail.com>
2026-08-21 21:49:03 +0200

    modif page.tsx et layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 "use client"
 
 import { useState } from "react";
+import api from "./api";
 
 // je crée 1 type qui represente chaque transaction 
 type Transaction = {
@@ -15,8 +16,25 @@ type Transaction = {
 
 export default function Home() {
   // je cree 1 tableau de transaction
-  // dns usestate je precise ce que le tableau transaction va contenir la un tableau
+  // dns usestate je precise ce que le tableau transaction va contenir un tableau qui par default ne contient rien
   const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  // je cree 1 fonction pr chargé 1 transaction
+  // assync pr appelé 1 api
+  const getTransactions = async()=>{
+    // try et catch pr attraper 1 erreur quand il yen aura
+    try{
+      // j appel l api qui provint du fichier api.ts
+      // get pr recup la transaction
+      const res = await api.get<Transaction[]>("transactions/")
+
+      // je recup le tableau const [transactions, setTransactions]
+      setTransactions(res.data) 
+    }catch (error){
+
+    }
+  }
+
   return (
       <button className="btn btn-sm">
         test
